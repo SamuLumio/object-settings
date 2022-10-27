eef1bbbc66bb457724806e1527c639a674bed91d
MrHole <samu.lumio@outlook.com>
2022-10-27 22:15:23 +0300

    Moved GUI test with the others

---
+++ tests/test_gui_(manual).py
@@ -0,0 +1,44 @@
+import os
+
+import settings
+
+
+def _setup():
+	download_options = settings.Section("Downloader settings")
+	settings.Number("Speed limit", 5, section=download_options)
+	settings.Path("Target directory", os.path.expanduser('~'), has_to_exist=True, section=download_options)
+	settings.Choice("Mirror", ["Europe", "Asia", "North America", "South America", "Africa"], "Asia", section=download_options)
+	settings.Multichoice("Files to download", ["Video", "Audio", "Subtitles"], ["Video", "Audio", "Subtitles"], section=download_options)
+
+	ui_options = settings.Section("UI settings")
+	settings.Toggle("Show file descriptions", True, ui_options)
+	settings.Choice("Speed measurement unit", ['Mbit', 'MB'], 'Mbit', ui_options)
+
+
+def tkinter():
+	_setup()
+
+	import tkinter
+	window = tkinter.Tk()
+	window.title("Settings demo")
+
+	import settings_gui.tkinter
+	settings_gui.tkinter.SettingsFrame(window).pack(padx=5, pady=5, fill='both')
+
+	window.mainloop()
+
+
+
+def ttk():
+	_setup()
+
+	import tkinter
+	window = tkinter.Tk()
+	window.title("Settings demo")
+
+	import settings_gui.ttk
+	import sv_ttk
+	sv_ttk.set_theme('dark')
+	settings_gui.ttk.SettingsFrame(window).pack(padx=5, pady=5, fill='both')
+
+	window.mainloop()
