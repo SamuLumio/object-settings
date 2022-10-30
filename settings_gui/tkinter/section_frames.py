a667eea0f3b1c82663f1bec46d2205faffc9e976
MrHole <samu.lumio@outlook.com>
2022-10-30 21:27:43 +0200

    (GUI) improve support for custom setting subclasses

--- settings_gui/tkinter/section_frames.py
+++ settings_gui/tkinter/section_frames.py
@@ -11,9 +11,12 @@ class SectionFrame(a.layer.Frame):
 		super().__init__(master)
 		self.settings = []
 		for setting in section.settings:
-			frame = type_frames.types[type(setting)]
-			self.settings.append(frame(self, setting, autosave=autosave))
-			self.settings[-1].pack(fill='x', pady=PAD*2)
+			for type, type_frame in type_frames.types.items():
+				if isinstance(setting, type):
+					frame = type_frame(setting)
+					self.settings.append(frame(self, setting, autosave=autosave))
+					self.settings[-1].pack(fill='x', pady=PAD*2)
+					break
 
 	def save_settings(self):
 		for setting_frame in self.settings:
