8c1ef0db7705da93598ff10f16ee5d3a4da49c5b
MrHole <samu.lumio@outlook.com>
2023-04-10 19:33:59 +0300

    Improved datatype loading and created tests for it

--- settings/backend/datatype_loader.py
+++ settings/backend/datatype_loader.py
@@ -4,20 +4,22 @@ str_converter_functions = {
 	str: str,
 	int: int,
 	float: float,
-	bool: lambda string: configparser.ConfigParser.BOOLEAN_STATES[string],
+	bool: lambda string: configparser.ConfigParser.BOOLEAN_STATES[string.lower()],
 	list: lambda string: string.removeprefix("['").removesuffix("']").split("', '")
 }
 
 def get(data, output_type: type[str | int | float | bool | list]):
 	if isinstance(data, output_type):
 		return data
+	elif isinstance(data, float) and output_type == int:
+		return int(data)
 	elif isinstance(data, str) and output_type in str_converter_functions:
 		function = str_converter_functions[output_type]
 		return function(data)
 	else:
-		raise UnsupportedDataError(type(data))
+		raise UnsupportedDataError(type(data), output_type)
 
 
 class UnsupportedDataError(BaseException):
-	def __init__(self, data_type: type):
-		super().__init__(f"loading for source data type {data_type} is unsupported")
+	def __init__(self, data_type: type, wanted_type: type):
+		super().__init__(f"loading {data_type} source data as {wanted_type} is unsupported")
---
+++ tests/test_datatype_loader.py
@@ -0,0 +1,39 @@
+import settings
+
+load = settings.backend.datatype_loader.get
+
+
+def test_str():
+	assert load("", str) == ""
+	assert load("Test", str) == "Test"
+
+
+def test_int():
+	assert load(0, int) == 0
+	assert load(5, int) == 5
+	assert load("0", int) == 0
+	assert load("5", int) == 5
+
+
+def test_float():
+	assert load(0.0, float) == 0
+	assert load(1.5, float) == 1.5
+	assert load("0.0", float) == 0
+	assert load("1.5", float) == 1.5
+
+
+def test_bool():
+	assert load(True, bool) == True
+	assert load(False, bool) == False
+	assert load("True", bool) == True
+	assert load("False", bool) == False
+	assert load("yes", bool) == True
+	assert load("no", bool) == False
+
+
+def test_list():
+	assert load([], list) == []
+	LIST = ["test", "1", "2"]
+	assert load(LIST, list) == LIST
+	assert load(str(LIST), list) == LIST
+
